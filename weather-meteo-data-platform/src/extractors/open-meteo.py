fc62fe780cf754b57ca01799b7585d57b760cd0b
tgkiet <giakiettran14102005@gmail.com>
2026-04-15 14:30:23 +0700

    feat: implement weather meteo data retrieval with retry logic in OpenMeteoExtractor

--- weather-meteo-data-platform/src/extractors/open-meteo.py
+++ weather-meteo-data-platform/src/extractors/open-meteo.py
@@ -1,7 +1,33 @@
-Class OpenMeteoExtractor:
+import requests
+import time
+
+class OpenMeteoExtractor:
     def __init__(self, url: str):
         self.url = url
     
-    def get_air_quality_data(self):
-        
-        
+    def get_air_quality_data(self, params: dict):
+        max_retries = 3
+        retry_delay = 5  # seconds
+        for i in range(max_retries):
+            try:
+                response = requests.get(self.url, params=params)
+                response.raise_for_status() #check status 
+                return response.json() # dua vao {}, ma python tu ep kieu tu json sang dict
+            except Exception as e:
+                print(f"Error fetching data: {e}")
+                time.sleep(retry_delay)
+        raise Exception(f"Failed to fetch data after {max_retries} attempts")
+    
+# Example usage:
+if __name__ == "__main__":
+    # url = "https://archive-api.open-meteo.com/v1/archive"
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+            "latitude": 10.7756, "longitude": 106.7019, # Vị trí của TP.HCM
+            "current_weather": "true",
+            "timezone": "Asia/Bangkok", # Check xem co can timezone khong
+        }
+    
+    extractor = OpenMeteoExtractor(url)
+    data = extractor.get_air_quality_data(params)
+    print(data)
